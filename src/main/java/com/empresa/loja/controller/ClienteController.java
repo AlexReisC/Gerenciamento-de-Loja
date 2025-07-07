55cd4dac133bc507be38ee7272db39f37108f27e
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-07-06 23:54:15 -0300

    refactor: atualizar método registrarCliente para usar ClienteDTO e simplificar conversão

--- src/main/java/com/empresa/loja/controller/ClienteController.java
+++ src/main/java/com/empresa/loja/controller/ClienteController.java
@@ -1,13 +1,14 @@
 package com.empresa.loja.controller;
 
+import com.empresa.loja.dtos.response.ApiResponse;
+import com.empresa.loja.dtos.response.ClienteResponse;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.empresa.loja.dtos.request.RegistroClienteDTO;
+import com.empresa.loja.dtos.request.ClienteDTO;
 import com.empresa.loja.entity.Cliente;
 import com.empresa.loja.service.ClienteService;
 
@@ -24,20 +25,19 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> registrarCliente(@Valid @RequestBody RegistroClienteDTO registroClienteDTO) {
-        Cliente cliente = new Cliente(
-            registroClienteDTO.nome(),
-            registroClienteDTO.email(),
-            registroClienteDTO.senha(),
-            registroClienteDTO.cpf(),
-            registroClienteDTO.telefone(),
-            registroClienteDTO.dataCadastro(),
-            registroClienteDTO.endereco()
-        );
+    public ResponseEntity<Object> registrarCliente(@Valid @RequestBody ClienteDTO clienteDTO) {
+        Cliente cliente = convertDtoToCliente(clienteDTO);
         clienteService.salvarCliente(cliente);
         return new ResponseEntity<>("Cliente cadastrado com sucesso!", HttpStatus.CREATED);
     }
 
-
-    
+    private Cliente convertDtoToCliente(ClienteDTO clienteDTO){
+        return new Cliente(
+                clienteDTO.nome(),
+                clienteDTO.email(),
+                clienteDTO.cpf(),
+                clienteDTO.telefone(),
+                clienteDTO.endereco()
+        );
+    }
 }
