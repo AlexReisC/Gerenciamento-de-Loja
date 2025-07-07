86f54bf44ef563713bea2115fa7f9c99dad89f11
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-07-06 23:55:08 -0300

    refactor: improve error handling and add logging

--- src/main/java/com/empresa/loja/service/ClienteService.java
+++ src/main/java/com/empresa/loja/service/ClienteService.java
@@ -1,8 +1,9 @@
 package com.empresa.loja.service;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import com.empresa.loja.repository.EnderecoRepository;
 
 @Service
 public class ClienteService {
+    private static final Logger logger = LoggerFactory.getLogger(ClienteService.class);
     @Autowired
     private ClienteRepository clienteRepository;
 
@@ -20,24 +22,23 @@ public class ClienteService {
     private EnderecoRepository enderecoRepository;
 
     public void salvarCliente(Cliente cliente){
+        logger.info("Iniciando cadastro do cliente");
         Optional<Cliente> clienteByEmail = clienteRepository.findByEmail(cliente.getEmail());
+
         if (clienteByEmail.isPresent()) {
-            throw new ClientAlreadyExistsException("Já existe um cliente com o email: " + cliente.getEmail());
-        }
-        
-        Optional<Cliente> clientebyCpf = clienteRepository.findByCpf(cliente.getCpf());
-        if (clientebyCpf.isPresent()) {
-            throw new ClientAlreadyExistsException("Já existe um cliente com o cpf: " + cliente.getCpf());
+            logger.error("Erro ao cadastrar cliente, email {} em uso por outro cliente.", cliente.getEmail());
+            throw new ClientAlreadyExistsException("Este email está em uso por outro cliente");
         }
 
-        LocalDateTime dataCadastro = cliente.getDataCadastro();
-        if (dataCadastro == null) {
-            dataCadastro = LocalDateTime.now();
-            cliente.setDataCadastro(dataCadastro);
+        Optional<Cliente> clientebyCpf = clienteRepository.findByCpf(cliente.getCpf());
+        if (clientebyCpf.isPresent()) {
+            logger.error("Erro ao cadastrar cliente, cpf {} em uso por outro cliente ", cliente.getCpf());
+            throw new ClientAlreadyExistsException("Este CPF já está em uso por outro cliente");
         }
 
         enderecoRepository.save(cliente.getEndereco());
         clienteRepository.save(cliente);
+        logger.info("Cliente cadastrado.");
     }
 
     public Cliente buscarClientePeloID(Long id){
