7a506166cd34387c5176fdd3dcbe18178a7a7942
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-08-23 19:18:46 -0300

    feat: add method to find Pedido by Cliente ID in PedidoRepository

--- src/main/java/com/loja/sistema/pedido/PedidoRepository.java
+++ src/main/java/com/loja/sistema/pedido/PedidoRepository.java
@@ -1,6 +1,8 @@
 package com.loja.sistema.pedido;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    Optional<Pedido> findByCliente(Long id);
 }
